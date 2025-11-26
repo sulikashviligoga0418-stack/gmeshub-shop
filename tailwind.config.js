72d747dd75f815b846954f6ace4800af3b2e6efe
sulikashviligoga0418-stack <sulikashviligoga0418@gmail.com>
2025-11-26 22:23:28 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // <--- ეს დავამატეთ, რომ ღამის რეჟიმი მართო
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
